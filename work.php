1109c5de113ba53047f04b6f10fef92facb6652f
Dragos <dpop1990@gmail.com>
2016-07-08 17:00:39 +0300

    reverted !!!!!

--- work.php
+++ work.php
@@ -1,7 +1,9 @@
 <?php
 use RedBeanPHP\R;
 
+$serverBase = "http://localhost/AutoUpdateServer";
 R::setup('mysql:host=localhost;dbname=testdbClient','root','');
+
 try
 {
     $lastRow =  R::getRow("select * from versions order by `id` desc limit 1");
@@ -14,19 +16,71 @@ catch (Exception $ex)
 }
 
 // make request to server
-$versionsList = CallAPI("GET","http://localhost/AutoUpdateServer/getVersion.php",array("clientVersion" => $lastRow->getID()));
+$versionsResponse = CallAPI("GET","http://localhost/AutoUpdateServer/getVersion.php",array("clientVersion" => $lastRow->getID()));
 
+$versionsList = unserialize($versionsResponse);
 
-foreach ($versionsList as $version)
+if (is_array($versionsList) || is_object($versionsList))
 {
-    echo $version["downloadUrl"]. " <br />";
+    foreach ($versionsList as $version)
+    {
+        try
+        {
+            $zip = new ZipArchive;
+            if(get_http_response_code($serverBase.$version["downloadUrl"]) != "200"){
+                echo "Could not get version ". $serverBase.$version["downloadUrl"];
+            }else{
+                file_put_contents('./downloads/tmp.zip',file_get_contents($serverBase.$version["downloadUrl"]));
+                $zip = new ZipArchive();
+                $res =$zip->open("./downloads/tmp.zip");
+                if ($res === TRUE) {
+                    $id = $version["id"];
+                    $versionTempPath = './downloads/temp/'.$id."/";
+                    mkdir($versionTempPath);
+                    $zip->extractTo($versionTempPath);
+                    $zip->close();
+                    recurse_copy($versionTempPath, ".");
+                } else {
+                    echo 'doh!';
+                }
+            }
+
+
+        }catch (Exception $ex)
+        {
+            echo "Could not get version ". $serverBase.$version["downloadUrl"];
+        }
+    }
+}
+else
+{
+    echo "nope";
 }
 
+
+function recurse_copy($src,$dst) {
+    $dir = opendir($src);
+    @mkdir($dst);
+    while(false !== ( $file = readdir($dir)) ) {
+        if (( $file != '.' ) && ( $file != '..' )) {
+            if ( is_dir($src . '/' . $file) ) {
+                recurse_copy($src . '/' . $file,$dst . '/' . $file);
+            }
+            else {
+                copy($src . '/' . $file,$dst . '/' . $file);
+            }
+        }
+    }
+    closedir($dir);
+}
 
 // start patching the current code
 
 
-
+function get_http_response_code($url) {
+    $headers = get_headers($url);
+    return substr($headers[0], 9, 3);
+}
 
 function CallAPI($method, $url, $data = false)
 {
